c8bbe2eb07397d945c065fc800d7f799ba2d5368
Alvaro <dralvphd2@gmail.com>
2022-08-03 16:16:51 -0300

    instanciando

---
+++ padroesDeProjetoJava/src/singletonpack/SingletonEager.java
@@ -0,0 +1,14 @@
+package singletonpack;
+
+public class SingletonEager {
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstancia(){
+
+        return instancia;
+    }
+}
